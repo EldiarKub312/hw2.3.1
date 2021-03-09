8d45dd4c21620c89ba9b7ffd1bd76345b11bd839
EldiarKubanychbekov <eldiar6442003@gmail.com>
2021-03-10 00:14:11 +0600

    Initial commit

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,25 @@
+package com.company;
+
+class BankAccount extends Exception {
+
+    private double amount;
+
+    public void deposite(double sum) {
+        amount += sum;
+        return;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double withDraw(double sum) throws LimitException {
+        if (sum > amount) {
+            throw new LimitException("WARNING! АКЧА АЗ КАЛДЫ!", getAmount());
+        }
+        amount -= sum;
+        System.out.println("Картадан алынчу сумма:" + sum);
+
+        return amount;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposite(20000);
+
+        System.out.println("Нажмите на ENTER ");
+        Scanner scanner = new Scanner(System.in);
+        String y = scanner.nextLine();
+
+
+        while(true){
+            try{
+                System.out.println("---------------------");
+                System.out.println("Сиздин картадагы сумма:" + bankAccount.getAmount());
+                bankAccount.withDraw(6000);
+            }catch (LimitException s){
+                System.out.println(s.getMessage());
+                try {
+                    bankAccount.withDraw((int) bankAccount.getAmount());
+                    System.out.println("---------------------");
+                    System.out.println("Сиздин картадагы сумма:" + bankAccount.getAmount());
+
+                }catch (LimitException limitException){
+                    limitException.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+}
